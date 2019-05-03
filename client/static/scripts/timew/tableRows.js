d89258b2a37762cba42364db223187d23f0835da
Evan Vander Hoeven <vhevan.927@gmail.com>
2019-05-03 15:38:40 -0500

    completed timewarrior base functionality
    there is room for improvement, but you can start timers, end timers, and review timers from the website without need for the terminal anymore.

--- client/static/scripts/timew/tableRows.js
+++ client/static/scripts/timew/tableRows.js
@@ -1,9 +1,6 @@
 
 //Taken from: https://stackoverflow.com/a/17147973
 jQuery(document).ready(function ($) {
-    $('.clickable-row').click(function () {
-        window.location = $(this).data('href')
-    })
     $('#allTimersTable').DataTable({
         order: [[0, 'asc']]
     })
--- client/static/scripts/timew/timers.js
+++ client/static/scripts/timew/timers.js
@@ -3,7 +3,8 @@
  *        that has the input tags.
  * @param {string} tags An string encoding an array of tags
  */
-function endTask(tags) {
+function endTimer(tags) {
+  alert(`endTask entered. Tags = ${tags}`)
   // turning the string into an array
   let tagsArr = tags.split(', ')
   // make a request to the API to end the task selected
@@ -14,10 +15,10 @@ function endTask(tags) {
       args: tagsArr
     },
     success: (success, stdout, stderr) => {
-      // IDK what to do with this...
+      location.reload()
     },
-    error: (success, stdout, stderr) => {
-      // TODO ask Austin what I should do with this.
+    error: (httpRequest, error) => {
+      alert(`Error connecting to API:\n${error}`)
     },
     dataType: 'json',
     timeout: 10000
@@ -28,27 +29,27 @@ function endTask(tags) {
  * @brief sends a post request to the API to start a timer
  * @return none
  */
-function startTask() {
+function startTimer() {
   let tagsInput = document.getElementById('timew-tags')
   let tagsStr = tagsInput.value
   let tags = tagsStr.split(',')
-  tags.array.forEach(element => {
+  tags.forEach((element) => {
     // Cleaning the input of whitespace
-    element.string.trim()
-    // Input doesn't need to be sanitize because that
+    element.trim()
+    // Input doesn't need to be sanitized because that
     // is monitored server side, but that would go here.
   })
   $.ajax({
     type: 'POST',
-    url: '/timewAPI/stop',
+    url: '/timewAPI/start',
     data: {
       args: tags
     },
     success: (success, stdout, stderr) => {
-      // WIP
+      location.reload()
     },
-    error: (success, stdout, stderr) => {
-      // WIP
+    error: (httpRequest, error) => {
+      alert(`Error connecting to API:\n${error}`)
     },
     dataType: 'json',
     timeout: 10000
